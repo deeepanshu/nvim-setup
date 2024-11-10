b1dae18d2ce2c5b9cbbeaa3f19242fcb16cf264c
Deepanshu Jain <deepanshu.jain@agoda.com>
2024-11-10 12:47:55 +0700

    install vim-tmux-navigator

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,15 @@ function setKeymaps(mode, key, command, desc)
   keymap.set(mode, key, command, { desc = desc, silent = true, noremap = true })
   -- code
 end
+
+
+setKeymaps('n', '<leader>sv', '<C-w>v', "Split window vertically")
+setKeymaps('n', '<leader>sh', '<C-w>s', "Split window horizontally")
+setKeymaps('n', '<leader>se', '<C-w>=', "Make splits equal")
+setKeymaps('n', '<leader>sx', '<cmd>close<CR>', "Close current split")
+
+setKeymaps('n', '<leader>to', '<cmd>tabnew<CR>', "Open new tab")
+setKeymaps('n', '<leader>tx', '<cmd>tabclose<CR>', "Close current tab")
+setKeymaps('n', '<leader>tn', '<cmd>tabn<CR>', "Go to next tab")
+setKeymaps('n', '<leader>tp', '<cmd>tabp<CR>', "Go to previous tab")
+setKeymaps('n', '<leader>tf', '<cmd>tabnew %<CR>', "Open current buffer in new tab")
