b00590bac855634224a27d056c7eb7b1721fee7e
Deepanshu Jain <deepanshu.jain@agoda.com>
2024-11-22 21:33:44 +0700

    fix neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,16 +6,57 @@ return {
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true })
+        end,
+        desc = "Explorer NeoTree (root)",
+      },
+    },
     config = function()
-      setKeymap("n", "<leader>e", ":Neotree focus<cr>", "Neotree Focus")
       require("neo-tree").setup({
-        close_if_last_window = false,
-        indent_marker = "│",
+        sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+        source_selector = {
+          sources = {
+            { source = "filesystem" },
+            { source = "document_symbols" },
+            { source = "buffers" },
+            { source = "git_status" },
+          },
+        },
+        document_symbols = {
+          follow_cursor = true,
+        },
         filesystem = {
+          bind_to_cwd = false,
+          filtered_items = {
+            hide_by_pattern = {
+              "*.iml",
+            },
+            always_show = {
+              ".gitlab",
+              ".gitignore",
+              ".gitlab-ci.yml",
+              ".config",
+              ".local",
+              "target",
+            },
+            never_show = {
+              ".DS_Store",
+            },
+          },
           follow_current_file = {
             enabled = true,
           },
+          group_empty_dirs = true,
+          scan_mode = "deep",
+          use_libuv_file_watcher = true,
         },
+        open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+        close_if_last_window = false,
+        indent_marker = "│",
         window = { width = 30 },
       })
     end,
